5f174066db8d4d7cfda52750bc9901d4af18c56e
Dabeluchukwu <dabeluc@gmail.com>
2024-05-31 10:28:10 -0700

    main update

--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -37,8 +37,8 @@ function Hero() {
             {/* Button 👇*/}
             <div>
               <a
-                href="#footer"
-                // download={resume}
+                href={resume}
+                download={resume}
                 className="mt-6 text-green-400 btn rounded-none bg-inherit border-green-400 lg:btn-lg hover:bg-green-400 hover:text-white"
               >
                 Download CV
